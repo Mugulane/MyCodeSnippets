f7d5a01da02c2a731964e42dd0eb01f53ddd88b4
Andrus Rahni <anrahn@ttu.ee>
2024-04-21 18:21:55 +0300

    add: update Git directories all at once

---
+++ PS/UpdateGitRepositories.ps1
@@ -0,0 +1,21 @@
+﻿# Defineeri algkataloog, millest otsimist alustada. Näiteks: "C:\MinuProjektid"
+$algkataloog = "C:\Users\andrus\Documents\TalTech kool\Algoritmid ja struktuurid\Homework"
+
+# Käi läbi kõik alamkataloogid
+Get-ChildItem -Path $algkataloog -Directory -Recurse | ForEach-Object {
+    # Sisene igasse kataloogi
+    Push-Location $_.FullName
+
+    # Kontrolli, kas kataloog on Git repositoorium (.git kataloogi olemasolu järgi)
+    if (Test-Path -Path ".git") {
+        # Kui jah, käivita 'git pull'
+        Write-Host "Käivitan 'git pull' kataloogis $($_.FullName)"
+        git pull
+        "FinantsVabadus2021;"
+    }
+    
+    # Naase eelmisele tasemele
+    Pop-Location
+}
+
+Write-Host "Kõikide repositooriumite uuendamine on lõpetatud."
